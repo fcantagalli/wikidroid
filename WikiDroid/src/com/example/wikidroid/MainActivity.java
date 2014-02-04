3985164f15acd978d3efc3f981dcfa014c2af8f0
Kaiwen Xu <kevin@kevxu.net>
2014-02-03 20:41:03 -0500

    Add support library v4.
    Start to build navigation drawer and android-support-v4.jar.

--- WikiDroid/src/com/example/wikidroid/MainActivity.java
+++ WikiDroid/src/com/example/wikidroid/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.wikidroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	private String[] mListTitles = new String[] {"Hello", "Yay"};
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, mListTitles));
+        // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
 

3db4e3bd613ae36ddc26b4003b028176bfad356b
Felipe Tozato Cantagalli <felipe.cantagalli@gmail.com>
2014-02-06 13:06:23 -0500

    add the package related to the article searches. I have to deal with internet exceptions yet

---
+++ WikiDroid/src/cs408team3/wikisearch/QueryContentHolder.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wikisearch;
+
+/**
+ *
+ * @author Felipe
+ */
+public class QueryContentHolder {
+    String title;
+    String link;
+    String displayLink;
+
+    public QueryContentHolder(String title, String link, String displayLink) {
+        this.title = title;
+        this.link = link;
+        this.displayLink = displayLink;
+    }
+
+    @Override
+    public String toString() {
+        return "QueryContentHolder{" + "title=" + title + ", link=" + link + ", displayLink=" + displayLink + '}';
+    }
+    
+    
+    
+}

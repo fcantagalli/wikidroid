f606e71335bf24ca9f165d549d338c210e243f83
Felipe Tozato Cantagalli <felipe.cantagalli@gmail.com>
2014-02-06 18:57:00 -0500

    fixing package names

--- WikiDroid/src/cs408team3/wikisearch/HttpClientExample.java
+++ WikiDroid/src/cs408team3/wikisearch/HttpClientExample.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wikisearch;
+package cs408team3.wikisearch;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -72,7 +72,7 @@ public class HttpClientExample {
 
         // if none item are found, return null
         if (items.size() > 0) {
-            linksFounded = new ArrayList<>(items.size());
+            linksFounded = new ArrayList<QueryContentHolder>(items.size());
         } else {
             return null;
         }
--- WikiDroid/src/cs408team3/wikisearch/QueryContentHolder.java
+++ WikiDroid/src/cs408team3/wikisearch/QueryContentHolder.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package wikisearch;
+package cs408team3.wikisearch;
 
 /**
  *

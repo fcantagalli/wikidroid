64588bda2f7e4f9a25ac5997dba918a00dde08b5
Kaiwen Xu <kevin@kevxu.net>
2014-02-20 13:58:31 -0500

    Start to work on tab.

---
+++ WikiDroid/src/cs408team3/wikidroid/tab/TabManager.java
@@ -0,0 +1,15 @@
+package cs408team3.wikidroid.tab;
+
+public class TabManager {
+
+    private static final int DEFAULT_TAB_LIMIT = 10;
+
+    public TabManager(int tabSize) {
+
+    }
+
+    public TabManager() {
+        this(DEFAULT_TAB_LIMIT);
+    }
+
+}
